1d4d2e609e5402d841cdda489d2a208800fdf06f
mpaulweeks <mpaulweeks@gmail.com>
2017-07-27 17:24:11 -0400

    use my imports cause fuck me

--- server/server.go
+++ server/server.go
@@ -8,9 +8,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/trinchan/slackbot/Godeps/_workspace/src/github.com/gorilla/schema"
-	_ "github.com/trinchan/slackbot/importer"
-	"github.com/trinchan/slackbot/robots"
+	"github.com/mpaulweeks/slackbot/Godeps/_workspace/src/github.com/gorilla/schema"
+	_ "github.com/mpaulweeks/slackbot/importer"
+	"github.com/mpaulweeks/slackbot/robots"
 )
 
 func Main(robotMap map[string][]robots.Robot) {
